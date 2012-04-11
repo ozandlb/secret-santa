fe9bd66881401c7a97843f8285d206f0a4b52dc4
Sudeep Mathur <smathur01@gmail.com>
2012-04-11 16:15:52 +0800

    beginning transition to objects

--- secret_santa-js.js
+++ secret_santa-js.js
@@ -1,48 +1,86 @@
-var secretsanta = function(players) {
+function playerObj () {
+	this.playerId = null
+	this.partnerId = null;
+}
 
-	if (players < 3) {
+playerObj.prototype.setPartner(partnerId) {
+	this.partnerId = partnerId;
+}
+
+playerObj.prototype.getPartner() {
+	return this.partnerId;
+}
+
+playerObj.prototype.setPlayerId(playerId) {
+	this.playerId = playerId;
+}
+
+playerObj.prototype.getPlayerId() {
+	return this.playerId;
+}
+
+
+
+
+function secretSanta (numplayers) {
+
+	if (numplayers < 3) {
 		document.write("Error: Number of players must be greater than 3");
-		return;
+		return false;
+	}
+	
+	var this.numplayers = numplayers;
+	var this.remainplayers = this.numplayers;
+	var this.remaining = [];
+	var this.pairings = [];
+	var this.notavail = [];
+	var this.pairedwith = "";
+	var this.randomcounter = 0;
+
+	// initialize array of remaining players
+	for (var i=0;i<this.numplayers;i++) {
+		remaining[i] = new playerObj();
+		remaining[i].setPlayerId(i);
+		// remaining[i] = i+1;
 	}
 
-//	var remainplayers = players;
-//	var remaining = [];
-	var pairings = [];
-	var notavail = [];
-	var j = 0;
-	var randomcounter = 0;
+	document.write(remaining.toString());
 
-	// set up remaining players array
-//	for (var i=0;i<players;i++) {
-//		remaining[i] = i+1;
-//	}
+} // end function secretSanta
 
 
-	for (var i=1;i<=players;i++) {
+secretSanta.prototype.pairPlayers() {
 
-		if ((i==(players-1)) && (notavail[players] == null)) {
-			j = players;
+// 
+	for (var i=1;i<=this.numplayers;i++) {
+
+		// prevent last number from getting stuck with itself.
+		//   if second-to-last number sees that last number is unpaired, then
+		//     second-to-last number pairs with last number
+		if ((i==(this.numplayers-1)) && (notavail[this.numplayers] == null)) {
+			this.pairedwith = this.numplayers;
+			
 		}
 
 		else {
 
-			while (j = Math.ceil(Math.random()*players)) {
-				randomcounter++;
-				document.write("random: <b>" + j + "</b> random counter: "+ randomcounter + "<br>")
-				if ((j!=i) && (notavail[j] !== 1)) {
-					randomcounter=0;
+			while (this.pairedwith = Math.ceil(Math.random()*this.remainplayers)) {
+				this.randomcounter++;
+				document.write("random: <b>" + j + "</b> random counter: "+ this.randomcounter + "<br>")
+				if ((j!=i) && (this.notavail[j] !== 1)) {
+					this.randomcounter=0;
 					break;
 				}
 			}
 		}
 
-		pairings[i-1] = j;
-		notavail[j] = 1;
+		pairings[i-1] = pairedwith;
+		notavail[pairedwith] = 1;
 		
 		document.write(i + "=>" + pairings[i-1] + "<br>");
 
 	}
 	
-}
+} // end pairPlayers method
 
-secretsanta(1000);
+secretSanta(1000);
