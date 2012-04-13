987966454dd91288eab8018b6e86fcc7b61306ef
Sudeep Mathur <smathur01@gmail.com>
2012-04-13 13:27:23 +0800

    O(n) efficient algorith, with testing on

--- secret_santa-js.js
+++ secret_santa-js.js
@@ -1,86 +1,77 @@
-function playerObj () {
-	this.playerId = null
-	this.partnerId = null;
-}
-
-playerObj.prototype.setPartner(partnerId) {
-	this.partnerId = partnerId;
-}
-
-playerObj.prototype.getPartner() {
-	return this.partnerId;
-}
-
-playerObj.prototype.setPlayerId(playerId) {
-	this.playerId = playerId;
-}
-
-playerObj.prototype.getPlayerId() {
-	return this.playerId;
-}
-
-
-
-
 function secretSanta (numplayers) {
 
 	if (numplayers < 3) {
 		document.write("Error: Number of players must be greater than 3");
 		return false;
 	}
-	
-	var this.numplayers = numplayers;
-	var this.remainplayers = this.numplayers;
-	var this.remaining = [];
-	var this.pairings = [];
-	var this.notavail = [];
-	var this.pairedwith = "";
-	var this.randomcounter = 0;
+
+	var remainplayers = numplayers;
+	var remaining = [];
+	var pairings = [];
+	var partnerindex = null;
+	var randomcounter = 0;
+	var breakcounter=30;
 
 	// initialize array of remaining players
-	for (var i=0;i<this.numplayers;i++) {
-		remaining[i] = new playerObj();
-		remaining[i].setPlayerId(i);
-		// remaining[i] = i+1;
+	for (var i=1;i<=numplayers;i++) {
+		remaining[i-1] = i;
 	}
 
-	document.write(remaining.toString());
-
-} // end function secretSanta
-
-
-secretSanta.prototype.pairPlayers() {
-
-// 
-	for (var i=1;i<=this.numplayers;i++) {
+	// main loop to select pairs
+	for (var i=1;i<=numplayers;i++) {
 
 		// prevent last number from getting stuck with itself.
 		//   if second-to-last number sees that last number is unpaired, then
 		//     second-to-last number pairs with last number
-		if ((i==(this.numplayers-1)) && (notavail[this.numplayers] == null)) {
-			this.pairedwith = this.numplayers;
-			
+		if ((i==(numplayers-1)) && (pairings[numplayers-1] == null)) {
+
+			document.write("HIT SECOND TO LAST IF<br>");
+			// only two items left in array "remaining" -- last item will have index of 1
+			partnerindex = 1;
 		}
 
 		else {
 
-			while (this.pairedwith = Math.ceil(Math.random()*this.remainplayers)) {
-				this.randomcounter++;
-				document.write("random: <b>" + j + "</b> random counter: "+ this.randomcounter + "<br>")
-				if ((j!=i) && (this.notavail[j] !== 1)) {
-					this.randomcounter=0;
+			while (1) {
+
+				partnerindex = Math.floor(Math.random()*remainplayers);
+				
+				//test block
+				randomcounter++;
+				document.write("<b>while loop "+randomcounter+"</b>, random partnerindex: "+ partnerindex + " remaining[partnerindex]: <b>" + remaining[partnerindex] + "</b> random counter: "+ randomcounter + "<br>");
+
+				//infinite loop stopper	
+				if (randomcounter>breakcounter) {
+					document.write("HIT RUNAWAY LOOP STOPPER")
+					return;
+				}
+
+				if ((remaining[partnerindex]!=i) && (pairings[i-1] == null)) {
+					document.write("break!<br>")
 					break;
 				}
 			}
 		}
 
-		pairings[i-1] = pairedwith;
-		notavail[pairedwith] = 1;
+		document.write("random partnerindex: "+ partnerindex + " remaining[partnerindex]: <b>" + remaining[partnerindex] + "</b> random counter: "+ randomcounter + "<br>");
 		
+		// add matching partner to pairings array
+		pairings[i-1] = remaining[partnerindex];
 		document.write(i + "=>" + pairings[i-1] + "<br>");
+		document.write("pairings:"+pairings.toString() + "<br>");
+		
+		// truncate remaining array
+		remaining.splice(partnerindex, 1);
+		document.write("remaining: " + remaining.toString() + "<br>");
+
+		// reduce number of players
+		remainplayers--;
+		document.write("remainplayers: " + remainplayers + "<br>");
+
+		document.write("-------------<br>");
 
 	}
-	
-} // end pairPlayers method
 
-secretSanta(1000);
+}
+
+secretSanta(10);
